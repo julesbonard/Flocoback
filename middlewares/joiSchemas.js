b8e8e650173fbe622e91f2043d5cba93e68ab284
lucas <lucasleproust@gmail.com>
2019-12-05 13:20:14 +0100

    add miniFlora, statsOxygene, statsCity, statsTaxons, users in joiSchemas

--- middlewares/joiSchemas.js
+++ middlewares/joiSchemas.js
@@ -4,3 +4,24 @@ module.exports.statsOxygene = Joi.object({
   date: Joi.date().required(),
   rate: Joi.number().required()
 });
+module.exports.miniFlora = Joi.object({
+  number: Joi.number().required()
+});
+module.exports.statsCity = Joi.object({
+  district: Joi.number().required(),
+  street: Joi.number().required()
+});
+module.exports.statsTaxons = Joi.object({
+  number: Joi.number().required(),
+  restored: Joi.bool().required(),
+  status: Joi.string().required()
+});
+module.exports.users = Joi.object({
+  firstname: Joi.string().required(),
+  lastname: Joi.string().required(),
+  age: Joi.number().required(),
+  email: Joi.string().required(),
+  pseudo: Joi.string().required(),
+  password: Joi.string().required(),
+  avatar: Joi.string().required()
+});
