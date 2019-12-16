9e1a2974726879b9049a874f649367367911c411
lucas240 <53712391+lucas240@users.noreply.github.com>
2019-12-16 14:27:34 +0100

    Update associations.js

--- sequelize/associations.js
+++ sequelize/associations.js
@@ -14,73 +14,13 @@ const StatsCity = require("./models/statsCity");
 const StatsTaxons = require("./models/statsTaxons");
 const Tresaury = require("./models/tresaury");
 const User = require("./models/users");
-// Company.hasMany(User, { foreignKey: { allowNull: false} });
-// User.belongsTo(Company, { foreignKey: { allowNull: false}, onDelete: "CASCADE" });
 
-//MINIFLORA:
-// MiniFlora.hasMany(StatsTaxons, { foreignKey: { allowNull: false}});
-// StatsTaxons.belongsTo(MiniFlora, { foreignKey: { allowNull: false}});
-
-// MiniFlora.hasMany(StatsCity, { foreignKey: { allowNull: false}});
-// StatsCity.belongsTo(MiniFlora, { foreignKey: { allowNull: false}});
-
-// //USER
-
+//USER
 User.hasOne(Agenda, { foreignKey: { allowNull: false }, onDelete: "CASCADE" });
-// User.hasOne(Tresaury, { foreignKey: { allowNull: false}, onDelete: "CASCADE" })
-// User.hasOne(Friends, { foreignKey: { allowNull: false}, onDelete: "CASCADE" })
-// User.hasOne(Messages, { foreignKey: { allowNull: false}, onDelete: "CASCADE" })
-// User.hasOne(Comments, { foreignKey: { allowNull: false}, onDelete: "CASCADE" })
-// User.hasOne(Likes, { foreignKey: { allowNull: false}, onDelete: "CASCADE" })
 User.hasMany(Posts, { foreignKey: { allowNull: false }, onDelete: "CASCADE" });
-// User.hasMany(Pots, { foreignKey: { allowNull: false}, onDelete: "CASCADE"})
 
-// //AGENDA
+//AGENDA
 Agenda.belongsTo(User, { foreignKey: { allowNull: false } });
 
-// // // //TRESAURY
-// Tresaury.belongsTo(User, { foreignKey: { allowNull: false} })
-
-// // ​
-
-// // // //FRIENDS
-// Friends.belongsTo(User, { foreignKey: { allowNull: false} })
-
-// // ​
-
-// // // //MESSAGE
-// Messages.belongsTo(User, { foreignKey: { allowNull: false} })
-
-// // ​
-
-// // // //COMMENT
-// Comments.belongsTo(User, { foreignKey: { allowNull: false} })
-// Comments.belongsTo(Posts, { foreignKey: { allowNull: false} })
-
-// //LIKES
-// Likes.belongsTo(User, { foreignKey: { allowNull: false} })
-// Likes.belongsTo(Posts, { foreignKey: { allowNull: false} })
-
-// //POSTS
-
+//POSTS
 Posts.belongsTo(User, { foreignKey: { allowNull: false } });
-// Posts.hasMany(Comments, { foreignKey: { allowNull: false} })
-// Posts.hasMany(Likes, { foreignKey: { allowNull: false} })
-// // //POTS
-// Pots.belongsTo(User, { foreignKey: { allowNull: false} })
-// Pots.hasOne(Seeds, {foreignKey: { allowNull: false}})
-
-// // //SEEDS
-// Seeds.belongsTo(Pots, { foreignKey: { allowNull: false} })
-// Seeds.hasOne(Plants, { foreignKey: { allowNull: false} })
-// Seeds.hasMany(Partners, { foreignKey: { allowNull: false} })
-
-// // //PLANTS
-// Plants.belongsTo(Seeds, { foreignKey: { allowNull: false} })
-// Plants.hasOne(Locations, { foreignKey: { allowNull: false} })
-
-// // //LOCATIONS
-// Locations.belongsTo(Plants, { foreignKey: { allowNull: false} })
-
-// //PARTNER
-// Partners.hasMany(Seeds, { foreignKey: { allowNull: false} })
